390013f47d238c6de85d5dd42389476b3934ac65
zangandi <zangandi@gmail.com>
2021-11-15 00:18:34 -0600

    updates

--- processing/generate_training_data_deepod.m
+++ processing/generate_training_data_deepod.m
@@ -1,15 +1,17 @@
 clear all
 % load trips
-load('raw_training_trips.mat');
+load('..\embeddings\raw_training_trips.mat');
 % load edge information
-load('hd_maps_and_traffic_for_each_link_11_03.mat')
+load('..\embeddings\hd_maps_and_traffic_for_each_link_11_03.mat')
 % load road network embedding
 load('..\embeddings\edge_embedding_np.mat')
 % load HD maps embedding
 load('..\embeddings\hd_maps_embeddings.mat');
-
+%
+train_or_test = csvread('..\embeddings\embedtrain_test_label.csv');
 %
 experiment = cell(size(training_data,2),1);
+load('..\embeddings\experiment_1_deepod.mat')
 for i = 1:size(training_data,2)
     this_trip = training_data{i};
     %
@@ -23,6 +25,17 @@ for i = 1:size(training_data,2)
     %
     experiment{i}.y = this_trip.mdc;
     experiment{i}.x = x;
+    experiment{i}.train_or_test = train_or_test(i);
 end
 
 save('experiment_1_deepod.mat', 'experiment')
+
+load('..\embeddings\experiment_1_deepod.mat')
+for i = 1:size(training_data,2)
+    this_trip = training_data{i};
+    %
+    experiment{i}.number_of_edges = size(this_trip.cpath,2);
+    experiment{i}.train_or_test = train_or_test(i);
+end
+
+save('..\embeddings\experiment_1_deepod.mat', 'experiment')
